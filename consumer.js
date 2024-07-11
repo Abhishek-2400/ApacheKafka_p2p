0212ff262064d91d8609af40807ba42925324c2a
Abhishek-2400 <abhi2003shukla@gmail.com>
2024-07-11 18:15:46 +0530

    Added consumer and producer . Cli input enabled .

---
+++ consumer.js
@@ -0,0 +1,22 @@
+import { kafka } from './client.js'
+
+const group = process.argv[2];
+const init = async () => {
+
+    const consumer = kafka.consumer({ groupId: group })
+    await consumer.connect()
+    await consumer.subscribe({ topics: ['rider-updates'], fromBeginning: true })
+
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
+            console.log(
+                ` group:${group} topic: ${topic}  partition:${partition}  message:${message.value}`
+            )
+        },
+    })
+
+
+}
+
+init()
--- producer.js
+++ producer.js
@@ -1,8 +1,40 @@
-import { kafka } from './client'
+
+import { kafka } from './client.js'
+import readline from 'readline'
+
+
+const rl = readline.createInterface({  //creating a readline cli interface 
+    input: process.stdin,
+    output: process.stdout,
+});
+
 
 const init = async () => {
     const producer = kafka.producer()
     await producer.connect()
+
+    rl.setPrompt("Enter message>");  //setting promt 
+    rl.prompt();          //asking user to enter prompt
+
+    rl.on('line', async (line) => {
+        const [user_name, user_location] = line.split(" ");
+        await producer.send({
+            topic: 'rider-updates',
+            messages: [
+                {
+                    //north -->0 south -->1
+                    partition: user_location.toLowerCase() === "north" ? 0 : 1,
+                    key: 'location-update',
+                    value: JSON.stringify({ name: user_name, location: user_location })
+                }
+
+            ],
+        })
+    }).on('close', async () => {
+        await producer.disconnect();
+    })
+
+
 }
 
-inti()
+init()
